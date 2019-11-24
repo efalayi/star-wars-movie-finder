fe513579c7d18f6e455ec52e662e3f344e3549b2
Esther Falayi <esther.falayi@andela.com>
2019-11-24 21:54:46 +0100

    feat(filter): add gender filter
    - create gender filter component
    - create characters filter function
    - update scss styles

--- src/components/film/FilmCharactersTable.vue
+++ src/components/film/FilmCharactersTable.vue
@@ -1,61 +1,65 @@
 <template>
-  <table class="table table-bordered animated slower fadeIn">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Name</th>
-        <th scope="col">Gender</th>
-        <th scope="col">Height</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-if="!hasCharacters">
-        <td colspan="4">No characters exist for this movie</td>
-      </tr>
-      <tr
-        v-for="(character, index) in characters"
-        :key="character.id">
-        <th scope="row">{{ index + 1 }}</th>
-        <td>{{ character.name }}</td>
-        <td class="text-center">
-          <span v-if="genderIsUndefined(character.gender)">
-            {{ character.gender }}
-          </span>
-          <font-awesome-icon
-            v-else
-            :icon="character.gender"
-            size="lg">
-          </font-awesome-icon>
-        </td>
-        <td class="text-center">{{ character.height }}</td>
-      </tr>
-      <tr v-if="hasCharacters">
-        <td colspan="3">
-          <div class="flex items-center justify-between">
-            <span>Total Film Characters:</span>
-            <span class="text-md text-yellow">{{ totalFilmCharacters }}</span>
-          </div>
-        </td>
-        <td>
-          <div class="flex items-center justify-between">
-            <span>Total:</span>
-            <div class="flex flex-col">
-              <span class="text-sm text-yellow">
-              {{ sumOfHeights.cm }}<sub>cm</sub>
-              </span>
-              <div>(<span class="text-sm">
-                  {{ sumOfHeights.ft }}<sub>ft</sub>
+  <table class="film__table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Gender</th>
+          <th scope="col">Height</th>
+        </tr>
+      </thead>
+      <transition-group
+        appear
+        name="flipX"
+        tag="tbody"
+        :duration="400">
+        <tr v-if="!hasCharacters" key="has-no-characters">
+          <td colspan="4">No characters exist for this movie</td>
+        </tr>
+        <tr
+          v-for="(character, index) in characters"
+          :key="character.id">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ character.name }}</td>
+          <td class="text-center">
+            <span v-if="genderIsUndefined(character.gender)">
+              {{ character.gender }}
+            </span>
+            <font-awesome-icon
+              v-else
+              :icon="character.gender"
+              size="lg">
+            </font-awesome-icon>
+          </td>
+          <td class="text-center">{{ character.height }}</td>
+        </tr>
+        <tr v-if="hasCharacters" key="has-characters">
+          <td colspan="3">
+            <div class="flex items-center justify-between">
+              <span>Total Film Characters:</span>
+              <span class="text-md text-yellow">{{ totalFilmCharacters }}</span>
+            </div>
+          </td>
+          <td>
+            <div class="flex items-center justify-between">
+              <span>Total:</span>
+              <div class="flex flex-col">
+                <span class="text-sm text-yellow">
+                {{ sumOfHeights.cm }}<sub>cm</sub>
                 </span>
-                <span class="text-xs ml-3 mr-3">/</span>
-                <span class="text-sm">
-                  {{ sumOfHeights.inch }}<sub>inch</sub>
-                </span>)
+                <div>(<span class="text-sm">
+                    {{ sumOfHeights.ft }}<sub>ft</sub>
+                  </span>
+                  <span class="text-xs ml-3 mr-3">/</span>
+                  <span class="text-sm">
+                    {{ sumOfHeights.inch }}<sub>inch</sub>
+                  </span>)
+                </div>
               </div>
             </div>
-          </div>
-        </td>
-      </tr>
-    </tbody>
+          </td>
+        </tr>
+    </transition-group>
   </table>
 </template>
 
--- src/components/film/StarWarsFilm.vue
+++ src/components/film/StarWarsFilm.vue
@@ -2,10 +2,20 @@
   <div v-if="loading">
     <loader loadingText="loading star wars film"></loader>
   </div>
-  <div v-else-if="film" class="w-100 animated slow slideInUp">
+  <div
+    v-else-if="film"
+    :class="{
+      'film': true,
+      'animated': true,
+      'slower': true,
+      'slideInUp': true
+    }">
     <div v-if="hasOpeningCrawl" class="scrolling-text text-center">
       <h4>{{ film.openingCrawl }}</h4>
     </div>
+    <gender-filter
+      :selectedOption="genderOption"
+      @change="handleGenderOptionChange"></gender-filter>
     <film-characters-table
       :characters="filmCharacters">
     </film-characters-table>
@@ -13,29 +23,54 @@
 </template>
 
 <script>
+import filterCharacters from '@/lib/formatters/filterCharacters';
 import FilmCharactersTable from './FilmCharactersTable';
+import GenderFilter from './GenderFilter';
 import Loader from '../Loader';
 
 export default {
   name: 'StarWarsFilm',
   components: {
     FilmCharactersTable,
+    GenderFilter,
     Loader
   },
   props: {
     loading: Boolean,
     film: Object
   },
+  data() {
+    return {
+      genderOption: 'all'
+    };
+  },
   computed: {
     filmCharacters() {
-      const { film } = this;
-      return film ? film.characters : [];
+      const { film, genderOption } = this;
+      const characters = film ? film.characters : [];
+      const filterValue = genderOption === 'all' ? null : genderOption;
+      const filteredCharacters = filterCharacters(characters, 'gender', filterValue);
+      return filteredCharacters;
     },
     hasOpeningCrawl() {
       const { film } = this;
       const openingCrawl = film ? film.openingCrawl : '';
       return Boolean(openingCrawl);
     }
+  },
+  beforeUpdate() {
+    if (this.loading) {
+      this.genderOption = 'all';
+    }
+  },
+  methods: {
+    handleGenderOptionChange(option) {
+      this.genderOption = option;
+    }
   }
 };
 </script>
+
+<style lang="scss">
+@import "~@/scss/components/film";
+</style>

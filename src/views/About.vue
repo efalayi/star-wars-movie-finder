66bd3159b12d1baf96fb347a3fc97998a2887dc5
Esther Falayi <esther.falayi@andela.com>
2019-11-25 07:02:44 +0100

    chore(styles): update styles
    - fix layout styles

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h3 class="text-center">Find out more about your favorite Star Wars movies and characters.
+    <h3 class="text-center">Find out more about your favorite Star Wars movies and characters. <br>
       <span class="text-sm">May the force be with the you! 💪</span>
     </h3>
 

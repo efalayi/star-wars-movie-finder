c656bcda4e30d6ee613b36e160688d0ccf694b09
Esther Falayi <esther.falayi@andela.com>
2019-12-11 05:37:37 +0100

    fix(dependencies): uninstall font-awesome dependencies

--- src/vendor/font-awesome.js
+++
@@ -1,9 +0,0 @@
-import {
-  faCaretDown, faCaretUp, faFemale, faHeart, faMale, faSpinner
-} from '@fortawesome/free-solid-svg-icons';
-
-const FONT_AWESOME_ICONS = [
-  faCaretDown, faCaretUp, faFemale, faHeart, faMale, faSpinner
-];
-
-export default FONT_AWESOME_ICONS;
